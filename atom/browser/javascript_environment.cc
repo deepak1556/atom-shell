ee6142a72eff4c58db339c6b087b1d81b1fd0c68
deepak1556 <hop2deep@gmail.com>
2015-06-26 03:43:43 +0530

    v8: parse max_old_space_size during initilisation

--- atom/browser/javascript_environment.cc
+++ atom/browser/javascript_environment.cc
@@ -4,6 +4,9 @@
 
 #include "atom/browser/javascript_environment.h"
 
+#include <string>
+
+#include "atom/common/atom_command_line.h"
 #include "gin/array_buffer.h"
 
 namespace atom {
@@ -22,6 +25,17 @@ bool JavascriptEnvironment::Initialize() {
   gin::IsolateHolder::LoadV8Snapshot();
   gin::IsolateHolder::Initialize(gin::IsolateHolder::kNonStrictMode,
                                  gin::ArrayBufferAllocator::SharedInstance());
+
+  // Set max size of old heap space.
+  auto args = AtomCommandLine::argv();
+  for (const std::string& arg : args)
+    if (!strncmp(arg.c_str(),
+                 "--max_old_space_size=",
+                 sizeof("--max_old_space_size=") - 1)) {
+      v8::V8::SetFlagsFromString(arg.c_str(), arg.size());
+      break;
+    }
+
   return true;
 }
 
